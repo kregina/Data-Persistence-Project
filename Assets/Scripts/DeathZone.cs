aadbf9f2f5c05f9da0ef220bb9bb6279cd961b33
Kelly Silva <kellynha756@gmail.com>
2023-04-16 11:24:46 +0300

    persistence of the best player name and score,

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public MainManager Manager;
+    public UIMainScene mainScene;
 
     private void OnCollisionEnter(Collision other)
     {
         Destroy(other.gameObject);
-        Manager.GameOver();
+        mainScene.GameOver();
     }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputField playerNameInput;
+    private string playerName;
+
+    private void Start()
     {
-        
+        playerNameInput.onValueChanged.AddListener(OnInputFieldValueChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartNew()
     {
-        
+        MainManager.Instance.newPlayerName = playerName;
+
+        MainManager.Instance.SaveUserData();
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnInputFieldValueChanged(string value)
+    {
+        playerName = value;
+    }
+
+    public void Exit()
+    {
+        MainManager.Instance.SaveUserData();
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
